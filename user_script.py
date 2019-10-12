dba6cdfd471d96d86bbf9ba4922d92034f351c67
Melker Österberg <melkerosterberg@gmail.com>
2019-10-12 15:53:25 +0000

    wip

--- user_script.py
+++ user_script.py
@@ -2,7 +2,8 @@ from flask import Flask, request
 import sys
 import requests
 import json
-import pyplot
+import matplotlib.pyplot as plt
+
 
 if len(sys.argv) <= 1: exit("Please provide words to look for.")
 
@@ -12,3 +13,10 @@ words = sys.argv[1:]
 query = '?words=' + ','.join(words)
 result = requests.get(server_url + query).json()
 print(result)
+
+import numpy as np
+
+x = np.arange(0, 5, 0.1);
+y = np.sin(x)
+
+plt.plot(10, 5)
